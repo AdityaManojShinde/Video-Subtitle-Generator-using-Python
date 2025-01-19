ab3663c9755aa275364b135d4e20f4d24c1cbb86
Aditya <aditya.manoj.shinde@gmail.com>
2025-01-19 11:57:02 +0530

    using whisper base model to transcripte a simple audio

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-
+import whisper
 
 # Start Project execution
 def main() -> None:
     # add all the processes
-    pass
-
+    # basic working of the wisper base model
+    model = whisper.load_model("base")
+    result = model.transcribe("audio.mp3")
+    with open("script.txt", 'w') as file:
+        file.writelines(result["text"])
 
 if __name__ == "__main__":
     main()
