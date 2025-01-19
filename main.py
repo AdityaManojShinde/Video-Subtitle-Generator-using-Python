472f7184689f64cb8efd969dd66be232bc2d4396
Aditya <aditya.manoj.shinde@gmail.com>
2025-01-19 13:50:49 +0530

    Added changes to main.py to test the whisper model

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import whisper
 import torch
 
-# Start Project execution
-def main() -> None:
-    # add all the processes
-    # basic working of the wisper base model
+# Test Whisper model
+def test_whisper():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = whisper.load_model("base", device=device)
     result = model.transcribe("audio.mp3")
     with open("script.txt", 'w') as file:
         file.writelines(result["text"])
+
+# Start Project execution
+def main() -> None:
+    # add all the processes
+    # test basic working of the wisper base model
+    test_whisper()
+    
 
 if __name__ == "__main__":
     main()
