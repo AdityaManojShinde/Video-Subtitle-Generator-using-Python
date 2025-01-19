3835d75d2b9337b5b6a4e6e7e6429cb9cb34f084
Aditya <aditya.manoj.shinde@gmail.com>
2025-01-19 12:55:20 +0530

    Added checking for gpu and cpu in main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import whisper
+import torch
 
 # Start Project execution
 def main() -> None:
     # add all the processes
     # basic working of the wisper base model
-    model = whisper.load_model("base")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = whisper.load_model("base", device=device)
     result = model.transcribe("audio.mp3")
     with open("script.txt", 'w') as file:
         file.writelines(result["text"])
