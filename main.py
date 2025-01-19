f9a74262840ed3865364952f3a426abe81dad65e
Pushkar Kumar <pushkarkumar0997@gmail.com>
2025-01-19 16:57:40 +0530

    Hi mssg

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tests import test_whisper
 # Start Project execution
 def main() -> None:
     # add all the processes
-    # test basic working of the wisper base model
+    # test basic working of the whisper base model
     test_whisper()
     
 
