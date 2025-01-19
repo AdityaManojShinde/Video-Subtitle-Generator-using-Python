9653ea52d7cc0599e7d35b106632d3c2c53241a1
Aditya <aditya.manoj.shinde@gmail.com>
2025-01-19 16:29:22 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from tests import test_whisper
-
+#from tests import test_whisper
+from audio_ex import get_audio
 
 # Start Project execution
 def main() -> None:
     # add all the processes
+    get_audio()
     # test basic working of the wisper base model
-    test_whisper()
+    #test_whisper()
     
 
 if __name__ == "__main__":
